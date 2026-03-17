7defbb92bf90b36df87c5b310d99e55a368aefba
EsthefanyDev <esthefany.dev23@gmail.com>
2026-03-17 18:36:30 -0300

    feat: adicionado função de posições randômicas

--- jogo.js
+++ jogo.js
@@ -4,7 +4,25 @@ var largura = 0
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
-
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica(){
+    //criando posisões randômicas
+    var posicaox = Math.floor(Math.random() * largura) - 90
+    var posicaoy = Math.floor(Math.random() * altura) - 90
+    posicaox = posicaox < 0 ? 0 : posicaox
+    posicaoy = posicaoy < 0 ? 0 : posicaoy
+    console.log(posicaox, posicaoy)
+
+    // Criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaox + 'px'
+    mosquito.style.top = posicaoy + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
