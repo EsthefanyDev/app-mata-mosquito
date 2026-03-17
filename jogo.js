845b050cb76686e9d41b885d3204816366423e0d
EsthefanyDev <esthefany.dev23@gmail.com>
2026-03-17 19:11:16 -0300

    feat: adiciona a função de tamanhos randômicos

--- jogo.js
+++ jogo.js
@@ -19,10 +19,27 @@ function posicaoRandomica(){
     // Criar o elemento html
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosquito.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaox + 'px'
     mosquito.style.top = posicaoy + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+    console.log(tamanhoAleatorio())
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+    console.log(classe)
+
+    switch (classe){
+        case 0:
+            return 'mosquito1'
+
+        case 1:
+            return 'mosquito2'
+        
+        case 2:
+            return 'mosquito3'
+    }
 }
