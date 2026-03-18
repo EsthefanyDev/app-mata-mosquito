e60f70b1903382c50a4b9aff1b371899888f8823
EsthefanyDev <esthefany.dev23@gmail.com>
2026-03-18 09:44:58 -0300

    feat: adiciona a tela de game over com redirecionamento para reinício do jogo

--- jogo.js
+++ jogo.js
@@ -16,7 +16,7 @@ function posicaoRandomica(){
 
         //console.log('O lemento selecionado foi: v' + vidas)
         if (vidas > 3){
-            alert('Interromper o jogo')
+            window.location.href = 'fim_de_jogo.html'
         } else {
             document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
             vidas++
